cf2fbd5e278ce619a3744b244e0a2c18d535b2bd
Sean Harvey <sean@silverstripe.com>
2017-05-05 15:02:36 +1200

    Allow configuring resque worker count

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,10 +10,10 @@ class deploynaut::service inherits deploynaut {
 	if $service_manage {
 		servicetools::install_systemd_unit { $service_name:
 			service_options => {
-			  "Type" => "forking",
-			  "ExecStart" => "/usr/bin/php ${site_root}/framework/cli-script.php dev/resque/run queue=* count=4 flush=1",
-			  "User" => "www-data",
-			  "Restart" => "on-failure"
+				"Type" => "forking",
+				"ExecStart" => "/usr/bin/php ${site_root}/framework/cli-script.php dev/resque/run queue=* count=${service_workers} flush=1",
+				"User" => "www-data",
+				"Restart" => "on-failure"
 			},
 			service_ensure => $service_ensure,
 			service_enable => $service_enable
